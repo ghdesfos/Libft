3a4e05b565213770d013a13f2381f76fec20ff01
Ghislain Desfosses <ghdesfos@e1z4r13p9.42.us.org>
2018-12-15 22:10:29 -0800

    test5

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: ghdesfos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/22 22:10:22 by ghdesfos          #+#    #+#             */
-/*   Updated: 2018/12/15 22:06:54 by ghdesfos         ###   ########.fr       */
+/*   Updated: 2018/12/15 22:10:19 by ghdesfos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,19 @@ size_t		ft_strlcat(char *restrict dst, \
 {
 	size_t i;
 	size_t j;
-	size_t k;
+	size_t len_dst;
 
 	i = 0;
-	while (dst[i] != '\0')
-		i++;
 	j = 0;
-	while (src[j] != '\0' && j < size)
-	{
-		dst[i + j] = src[j];
-		j++;
-	}
-	dst[i + j] = '\0';
-	k = 0;
-	while (src[k] != '\0')
-		k++;
-	return (k + size);
+	len_dst = ft_strlen(dst);
+	while (dst[i] != '\0' && i < size)
+		i++;
+	while (src[j] != '\0' && i < size - 1)
+		dst[i++] = src[j++];
+	if (size != 0 && size >= len_dst)
+		dst[i] = '\0';
+	if (size < ft_strlen(dst))
+		return (ft_strlen(src) + size);
+	else
+		return (ft_strlen(src) + len_dst);
 }
